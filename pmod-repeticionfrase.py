4368cb2b47a77fd94f3fefd054115134f79f8fa4
Abrilskop <116703319+Abrilskop@users.noreply.github.com>
2022-11-11 06:52:59 -0500

    Add files via upload

---
+++ pmod-repeticionfrase.py
@@ -0,0 +1,15 @@
+# autor : Gianella Alexandra Ramos Ticahuanca
+# fecha: 10/11/2022
+# acción : Elaborar un algoritmo que utilizando un procedimiento imprima la cantidad de veces que le indiques una frase ingresada. 
+
+#Modulo especifico Repeticion
+def Repeticion(fras,canti):
+    cont=0   # 1 inicialización
+    while cont < canti:     # 2 condición
+          cont = cont + 1  # 3 actualización
+          print(fras) # 4 instrucción
+
+# Programa Principal
+frase=input("Ingresa la frase: ")
+cantidad=int(input("Ingrese la cantidad de veces que deseas repetirla: "))
+Repeticion(frase, cantidad)   # llamar al procedimiento Repeticio
